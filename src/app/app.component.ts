f2848803eebde013a5f89efaa137752482db7aa6
Abhishek <aj381997@gmail.com>
2022-03-06 01:18:06 +0530

    employee regestration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'employee';
+export class AppComponent implements OnInit {
+
+  exform: FormGroup;
+
+  ngOnInit() {
+
+  this.exform = new FormGroup({
+    'name' : new FormControl(null, Validators.required),
+    'email' : new FormControl(null, [Validators.required, Validators.email]),
+    'phone' : new FormControl(
+      null,
+      [
+        Validators.required,
+        Validators.pattern('^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$')
+      ]),
+    'message' : new FormControl(null, [Validators.required, Validators.minLength(10)])
+  });
+  }
+
+  clicksub() {
+    console.log(this.exform.value);
+    this.exform.reset();
+  }
+  get id() {
+    return this.exform.get('id');
+  }
+  get name() {
+    return this.exform.get('name');
+  }
+  get email() {
+    return this.exform.get('email');
+  }
+  get salary() {
+    return this.exform.get('salary');
+  }
+  get phone() {
+    return this.exform.get('phone');
+  }
+  get address() {
+    return this.exform.get('address');
+  }
+  get message() {
+    return this.exform.get('message');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
